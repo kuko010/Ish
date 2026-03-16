67ac29db845bc28b876a6b794a7042f25196528e
kuko010 <kukouwu@proton.me>
2026-03-16 17:43:48 +0100

    feat: config option

--- src/main/java/net/kuko/fabish/compat/config/ConfigOptions.java
+++ src/main/java/net/kuko/fabish/compat/config/ConfigOptions.java
@@ -3,4 +3,5 @@ package net.kuko.fabish.compat.config;
 public class ConfigOptions {
     // These are NOT static so GSON can save them
     public boolean enableLibTooltips = false;
+    public boolean enableFullbright = false;
 }
