e60861c6b1a62b9b69d17b4dc8ec90cbaca798be
kuko010 <kukouwu@proton.me>
2026-03-22 17:01:01 +0100

    remove: use logic

--- src/main/java/net/kuko/ish/registry/item/MarkerItem.java
+++ src/main/java/net/kuko/ish/registry/item/MarkerItem.java
@@ -5,9 +5,14 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.kuko.ish.IshClient;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.dedicated.Settings;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 
 public class MarkerItem extends Item {
@@ -17,17 +22,13 @@ public class MarkerItem extends Item {
 
 
     @Override
-    public InteractionResult useOn(UseOnContext useOnContext) {
-        if (useOnContext.getLevel().isClientSide()) {
-            BlockPos pos = useOnContext.getClickedPos();
-            Block block = useOnContext.getLevel().getBlockState(pos).getBlock();
-            if (!IshClient.contains(block)) {
-                IshClient.add(block);
-            } else {
-                IshClient.remove(block);
-            }
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand interactionHand) {
+        if (!level.isClientSide()) { // server
+
+        } else { // client
+
         }
 
-        return super.useOn(useOnContext);
+        return InteractionResultHolder.success(player.getItemInHand(interactionHand));
     }
 }
