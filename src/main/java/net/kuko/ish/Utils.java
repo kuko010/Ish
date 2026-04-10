8aca30713b5cc5bcc7a5873e883b9f84cfddd896
kuko¹ <139284780+kuko010@users.noreply.github.com>
2026-04-10 06:21:54 +0200

    Update Utils.java
    Clean up

--- src/main/java/net/kuko/ish/Utils.java
+++ src/main/java/net/kuko/ish/Utils.java
@@ -2,13 +2,5 @@ package net.kuko.ish;
 
 
 public class Utils {
-/*
-    public static int packARGB(long packed) {
-        return new Color(
-                (packed >> 16 & 255) / 255f,
-                (packed >> 8 & 255) / 255f,
-                (packed & 255) / 255f,
-                (packed >> 24 & 255) / 255f
-        ).argb();
-    }*/
+
 }
